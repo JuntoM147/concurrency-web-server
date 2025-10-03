ccb3d45a594099e2fa4ae6a5a29faf5aee6a07a7
Junto Mabuchi <juntomabuchi@hotmail.com>
2025-10-03 17:18:20 +0100

    Very basic http server impl

--- server.c
+++ server.c
@@ -7,6 +7,14 @@
 #include <arpa/inet.h>
 #include <string.h>
 
+
+int parse_request(char *input, char *method, char *path, char*version)
+{
+        sscanf(input, "%s %s %s", method, path, version);
+        return 0;
+}
+
+
 int main() {
         struct sockaddr_in server_addr;
         server_addr.sin_family = AF_INET;
@@ -36,17 +44,50 @@ int main() {
         struct sockaddr_in client_addr;
         socklen_t client_len = sizeof(client_addr);
         char buffer[1024];
-        char *msg = "HTTP/1.1 200 OK\r\n"
-                "Content-Type: text/plain\r\n"
-                "Content-Length: 12\r\n"
-                "\r\n"
-                "Hello World!";
+        char *msg;
+
+                // = "HTTP/1.1 200 OK\r\n"
+                // "Content-Type: text/plain\r\n"
+                // "Content-Length: 12\r\n"
+                // "\r\n"
+                // "Hello World!";
+                //
+        char method[8], path[64], version[16];
         
         while (1) {
                 int new_socket = accept(server_socket, (struct sockaddr *) &client_addr, &client_len);
 
                 read(new_socket, buffer, 1024);
-                printf("Recieved: %s\n", buffer);
+                parse_request(buffer, method, path, version);
+
+                if (strcmp(path, "/") == 0) {
+                        // Return index page
+                        msg = "HTTP/1.1 200 OK\r\n"
+                                "Content-Type: test/html\r\n"
+                                "Content-Length: 47\r\n"
+                                "\r\n"
+                                "<html><body><h1>Hello World!</h1></body></html>";
+                } else if (strcmp(path, "/hello") == 0) {
+                        msg = "HTTP/1.1 200 OK\r\n"
+                                "Content-Type: text/plain\r\n"
+                                "Content-Length: 12\r\n"
+                                "\r\n"
+                                "Hello World!";
+                } else {
+                        msg = "HTTP/1.1 404 Not found\r\n"
+                                "Content-Type: text/plain\r\n"
+                                "Content-Length: 48\r\n"
+                                "\r\n"
+                                "<html><body><h1>404 not found</h1></body></html>";
+                }
+                // printf("Recieved:\n%s\n", buffer);
+
+                // printf("Method: %s\n", method);
+                // printf("Path: %s\n", path);
+                // printf("Version: %s\n", version);
+                // printf("---------\n");
+                //
+                printf("Sending: %s\n", msg);
                 write(new_socket, msg, strlen(msg));
         }
 
